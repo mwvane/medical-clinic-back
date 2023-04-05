fa18cbb7e532c604d73b5e2ef33daad16e49cbc0
mwvane <bzishvili57@gmail.com>
2023-04-05 22:42:58 +0400

    Create Doctor model

---
+++ medical-clinic/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace medical_clinic.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [AllowNull]
+        public int? Views { get; set; }
+        [AllowNull]
+        public int? Rating { get; set; }
+        public bool IsPinned { get; set; } = false;
+    }
+}
