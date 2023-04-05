33573d9547a1d906158c6b9c36f5b9ebd51685a9
mwvane <bzishvili57@gmail.com>
2023-04-05 22:47:22 +0400

    Create Constants

---
+++ medical-clinic/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace medical_clinic
+{
+    public static class Constants
+    {
+        public const int JWT_TOKEN_VALIDITY_MINS = 30;
+        public const string JWT_SECURITY_KEY = "my security key...";
+        public static string DEFAULT_SERVER = "https://localhost:7129/";
+        public static string EMAIL_FROM = "laisha78@ethereal.email";
+        public static string MAIL_PASSWORD = "8NDtRWcmNYPrZf2TMW";
+
+        //minuts
+        public static int emailCodeValidDate = 30;
+
+    }
+}
