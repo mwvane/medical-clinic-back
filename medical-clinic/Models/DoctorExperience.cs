4d90fe9dc846c555f5c0360cb09e75e17a7eff2e
mwvane <bzishvili57@gmail.com>
2023-04-05 22:43:37 +0400

    Create DoctorExperience model

---
+++ medical-clinic/Models/DoctorExperience.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace medical_clinic.Models
+{
+    public class DoctorExperience
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]  
+        public int DoctorId { get; set; }
+        [Required]
+        public string Experience { get; set; }
+    }
+}
