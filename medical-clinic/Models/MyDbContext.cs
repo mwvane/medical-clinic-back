3fc25f89ea08d4efb954df13b8d60444acd9f24f
mwvane <bzishvili57@gmail.com>
2023-04-05 22:45:45 +0400

    Create MyDbContext

---
+++ medical-clinic/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace medical_clinic.Models
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<EmailConfirm> EmailConfirm { get; set; }
+        public DbSet<DoctorViews> DoctorViews { get; set; }
+        public DbSet<DoctorExperience> DoctorExperiences { get; set; }
+
+    }
+}
