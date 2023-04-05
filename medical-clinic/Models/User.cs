e46b2243e70a5fd41497c108b89c78c05bc89e7d
mwvane <bzishvili57@gmail.com>
2023-04-05 22:42:31 +0400

    Create User model

---
+++ medical-clinic/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace medical_clinic.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string IdentityNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; } = "Client";
+        [AllowNull]
+        public string? Token { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
