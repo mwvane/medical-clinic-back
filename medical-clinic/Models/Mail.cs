61a1f84bdb1998630452fa7d951209a007b4bb55
mwvane <bzishvili57@gmail.com>
2023-04-05 22:44:40 +0400

    Create Mail model

---
+++ medical-clinic/Models/Mail.cs
@@ -0,0 +1,13 @@
+﻿namespace medical_clinic.Models
+{
+    public class Mail
+    {
+        public List<string> EmailTo { get; set; } = new List<string>();
+        public string EmailFromId { get; set; } = "bzishvili57@gmail.com";
+        public string EmailFromPassword { get; set; } = "Ok Googlee";
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+        public bool IsBodyHtml { get; set; } = true;
+        public List<string>? Attachments { get; set; } = new List<string>();
+    }
+}
