8a10e33a15b25a46b0cbfa8d4761f0b102f1b17a
mwvane <bzishvili57@gmail.com>
2023-04-05 22:46:10 +0400

    Create EmailConfirm model

---
+++ medical-clinic/Models/EmailConfirm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace medical_clinic.Models
+{
+    public class EmailConfirm
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public DateTime ValidDate { get; set; }
+        
+    }
+}
