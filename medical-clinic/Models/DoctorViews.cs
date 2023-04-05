be400b0b51af6091b740f7f9f6afa159fc46fbdd
mwvane <bzishvili57@gmail.com>
2023-04-05 22:45:07 +0400

    Create DoctorView model

---
+++ medical-clinic/Models/DoctorViews.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace medical_clinic.Models
+{
+    public class DoctorViews
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int DoctorId { get; set; }
+    }
+}
