acbfa92fd9bb38de34b6f858d39a523a1742147b
mwvane <bzishvili57@gmail.com>
2023-04-08 15:00:05 +0400

    Update migrations

--- medical-clinic/Migrations/20230405204647_Add_Date_field_to_Book.Designer.cs
+++ medical-clinic/Migrations/20230405204647_Add_Date_field_to_Book.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using medical_clinic.Models;
 
@@ -11,9 +12,11 @@ using medical_clinic.Models;
 namespace medical_clinic.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230405204647_Add_Date_field_to_Book")]
+    partial class Add_Date_field_to_Book
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
