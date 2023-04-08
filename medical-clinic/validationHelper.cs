9e5437bc60472954e4293d4bf8873cfa834c5f54
mwvane <bzishvili57@gmail.com>
2023-04-08 14:59:54 +0400

    Create ValidationHelper

---
+++ medical-clinic/validationHelper.cs
@@ -0,0 +1,26 @@
+﻿using medical_clinic.Models;
+
+namespace medical_clinic
+{
+    public static class validationHelper
+    {
+        public static Result ValidateBook(Book book, MyDbContext context)
+        {
+            Result result = new Result() { Errors = new List<string>() };
+            if (book == null)
+            {
+                result.Errors.Add("ჯავშანი ცარიელია");
+            }
+            if (book.Date < DateTime.Now)
+            {
+                result.Errors.Add("მითითებულ დღეს დაჯავშნა შეუძლებელია!");
+            }
+            var isAlreadyBooked = context.Books.Where(item => item.DoctorId == book.DoctorId && item.Date == book.Date).FirstOrDefault() != null;
+            if (isAlreadyBooked)
+            {
+                result.Errors.Add("მითითებული დრო დაკავებულია!");
+            }
+            return result;
+        }
+    }
+}
