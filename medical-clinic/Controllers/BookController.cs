a29d018c7146de44bf547ab1b40bee9c8fdd8c8c
mwvane <bzishvili57@gmail.com>
2023-04-13 02:04:03 +0400

    Implement book update and remove functionality

--- medical-clinic/Controllers/BookController.cs
+++ medical-clinic/Controllers/BookController.cs
@@ -20,6 +20,9 @@ namespace medical_clinic.Controllers
         [HttpPost("addBook")]
         public Result AddBook([FromBody] Book book)
         {
+            book.Date = book.Date.ToLocalTime();
+            Console.WriteLine(book.Date);
+
             var res = validationHelper.ValidateBook(book, _context);
             if (res.Errors.Count > 0)
             {
@@ -27,22 +30,69 @@ namespace medical_clinic.Controllers
             }
             else
             {
+
                 _context.Books.Add(book);
                 _context.SaveChanges();
                 return new Result() { Res = book };
             }
         }
 
-        [HttpGet("getBookedDays")]
-        public Result GetBookedDays(int id = 6)
+        [Authorize]
+        [HttpPost("updateBook")]
+        public Result UpdateBook([FromBody] Book newBook)
+        {
+            var book = _context.Books.Where(item => item.Id == newBook.Id).FirstOrDefault();
+            if (book != null)
+            {
+                book.Date = newBook.Date;
+                book.Description = newBook.Description;
+                _context.Books.Update(book);
+                _context.SaveChanges();
+                return new Result() { Res = true };
+            }
+            return new Result() { Errors = new List<string>() { "მსგავსი ჯავშანი ვერ მოიძებნა" } };
+
+
+        }
+
+        [Authorize]
+        [HttpPost("removeBook")]
+        public Result RemoveBook([FromBody] int bookId)
+        {
+            var book = _context.Books.Where(item => item.Id == bookId).FirstOrDefault();
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+                return new Result() { Res = true };
+            }
+            return new Result() { Errors = new List<string>() { "მსგავსი ჯავშანი ვერ მოიძებნა" } };
+
+
+        }
+
+        [HttpGet("getDoctorBookedDays")]
+        public Result GetDoctorBookedDays(int id = 6)
         {
             Console.WriteLine(id);
             var bookedDays = _context.Books.Where(item => item.DoctorId == id).ToList();
-            if(bookedDays == null|| bookedDays.Count == 0)
+            if (bookedDays == null || bookedDays.Count == 0)
             {
                 return new Result() { Errors = new List<string>() { "ჩანაწერი ვერ მოიძებნა" } };
             }
-            return new Result() { Res = bookedDays};
+            return new Result() { Res = bookedDays };
+        }
+
+        [HttpGet("getClientBookedDays")]
+        public Result GetClientBookedDays(int id = 6)
+        {
+            Console.WriteLine(id);
+            var bookedDays = _context.Books.Where(item => item.UserId == id).ToList();
+            if (bookedDays == null || bookedDays.Count == 0)
+            {
+                return new Result() { Errors = new List<string>() { "ჩანაწერი ვერ მოიძებნა" } };
+            }
+            return new Result() { Res = bookedDays };
         }
 
     }
