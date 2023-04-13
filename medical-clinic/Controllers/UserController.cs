671371186fff377205dc792875b11798e2c92fcf
mwvane <bzishvili57@gmail.com>
2023-04-13 15:57:12 +0400

    Create UserController

---
+++ medical-clinic/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using medical_clinic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace medical_clinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public UserController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getUser")]
+        public Result GetUser(int id = 0)
+        {
+            if (id > 0)
+            {
+                var user = _context.Users.Where(item => item.Id == id).FirstOrDefault();
+                if (user != null)
+                {
+                    return new Result() { Res = user };
+                }
+                return new Result() { Errors = new List<string>() { "მსგავსი მომხმარებელი ვერ მოიძებმა" } };
+            }
+
+            else
+            {
+                return new Result() { Res = _context.Users.ToList() };
+            }
+        }
+    }
+}
