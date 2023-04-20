656d748d35f9c85b6e0a1c4f3706463f3c25af20
mwvane <bzishvili57@gmail.com>
2023-04-21 00:08:44 +0400

    Create CategoryFrint model

---
+++ medical-clinic/Models/CategoryFront.cs
@@ -0,0 +1,9 @@
+﻿namespace medical_clinic.Models
+{
+    public class CategoryFront
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public int DoctorCount { get; set; }
+    }
+}
